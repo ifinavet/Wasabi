16d6991512602fd4217ae39c377de0a9d5fc4a06
Christoffer Hennie <christoffer.hennie@outlook.com>
2024-09-30 12:58:31 +0200

    Add project files.

---
+++ IfiNavet.Web.Core/Program.cs
@@ -0,0 +1,31 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .AddAzureBlobMediaFileSystem() // This configures the required services for Media
+    .AddAzureBlobImageSharpCache() // This configures the required services for the Image Sharp cache
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseHttpsRedirection();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
