0778e65b56ba98bcee36f836e31129bc6c99b0de
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-02-16 14:58:24 +0100

    Update deployment workflow for Development environment: configure environment variables, adjust publish path, and set folder permissions

--- Wasabi/Program.cs
+++ Wasabi/Program.cs
@@ -21,9 +21,11 @@ builder.Services
         options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
     });
 
+/*
 #if DEVELOPMENT
 builder.Services.AddSassCompiler();
 #endif
+*/
 
 WebApplication app = builder.Build();
 app.UseXfo(options => options.SameOrigin());
