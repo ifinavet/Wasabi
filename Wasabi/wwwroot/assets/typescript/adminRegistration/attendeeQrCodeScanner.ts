ca14c6d8af8c86b1f332751401e85080b4b23605
Christoffer Hennie <christoffer.hennie@outlook.com>
2025-03-06 13:40:35 +0100

    Refactor QR code scanning implementation and update package dependencies

--- Wasabi/wwwroot/assets/typescript/adminRegistration/attendeeQrCodeScanner.ts
+++ Wasabi/wwwroot/assets/typescript/adminRegistration/attendeeQrCodeScanner.ts
@@ -1,9 +1,11 @@
+import {Html5QrcodeResult, Html5QrcodeScanner} from "html5-qrcode";
+
 const attendeeInfo = document.getElementById("scannedAttendee");
 const attendeeMemberIdFromInput = (<HTMLInputElement>document.getElementById("AttendeeMemberId"));
 const attendeeIdFromInput = (<HTMLInputElement>document.getElementById("AttendeeId"));
 const {attendees, eventId} = window.backendData;
 
-export const onScanSuccess = (decodedText: string, decodedResult: string) => {
+export const onScanSuccess = (decodedText: string, decodedResult: Html5QrcodeResult) => {
     // handle the scanned code as you like, for example:
     console.log(`Code matched = ${decodedText}`, decodedResult);
     let parsedData = JSON.parse(decodedText);
@@ -15,8 +17,13 @@ export const onScanSuccess = (decodedText: string, decodedResult: string) => {
     }
 };
 
-export const onScanFailure = (error: any) => {
-};
+
+export let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    {fps: 10, qrbox: {width: 250, height: 250}},
+    /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, () => {
+});
 
 (window as any).onScanSuccess = onScanSuccess;
-(window as any).onScanFailure = onScanFailure;
+(window as any).html5QrcodeScanner = html5QrcodeScanner;
